374c922c1ffb089fda87a35894ba7133c56c3bec
coutharuendl <101123673+Taruncodesinc@users.noreply.github.com>
2022-11-28 09:38:29 +0530

    Create 191.Number of bits.cpp

---
+++ 191.Number of bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ct=0;
+        while(n!=0){
+            if(n&1){
+                ct++;
+            }
+            n>>=1;
+        }
+        return ct;
+
+        
+    }
+};
